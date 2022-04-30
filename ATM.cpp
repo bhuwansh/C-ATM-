1c5abc273cb3a6b10495b28fe831c537fa422865
bhuwan <bhuwan@anandcoherentcare.onmicrosoft.com>
2022-04-30 14:49:44 +0530

    update

--- ATM.cpp
+++ ATM.cpp
@@ -1,55 +1,50 @@
 #include <iostream>
 using namespace std;
 
+void showMenu()
+{
+    // cout << "\n\n0:Account Open
+    //   n1:Display All
+    //   n2:By Account No
+    //   n3:Deposit
+    //   n4:Withdraw
+    //   n5:Exit" << endl;
+
+    cout << "####################################################" << endl;
+    cout << "######         *Welcome to AIS ATM*           ######" << endl;
+    cout << "##  -------------------------------------------   ##" << endl;
+    cout << "##  0) Register a user / Account Open             ##" << endl;
+    cout << "##  1) Withdraw INR                               ##" << endl;
+    cout << "##  2) Withdraw INR                               ##" << endl;
+    cout << "##  3) Withdraw International Currency            ##" << endl;
+    cout << "##  4) Check Balance                              ##" << endl;
+    // cout << "##  5) Check Balance                              ##" << endl;
+    cout << "####################################################" << endl;
+}
+
 // class
 class Bank
 {
 private:
-    // 1  user registration
+    int acno;
     char name[30];
     char Email[50];
     char Mobile[12];
-    int Age, WithdrawalFees;
-    char Gender[1];
-    double MonthlySalary, balance = 5000.00, WithdrawAmount;
-    int acno;
-    // 3 International currency withdrawal
+    int age;
+    string Gender;
+    int WithdrawalFees;
+    long MonthlySalary, balance, WithdrawAmount;
     string InternationalCurrency[3] = {"USD", "GBP", "AUD"};
-    // 4  Check balance
-   
 
 public:
-
-void GenerateAccNo(){
-    acno =  rand();
-}
-
     void OpenAccount()
     {
-     cout << "User Registration / Account Opening Form  :" << endl;
-
-            cout << " Enter Your Name  :";
-            cin >> name;
-            cout << endl;
-            cout << " Enter Your Email  :";
-            cin >> Email;
-            cout << endl;
-            cout << " Enter Your Mobile:";
-            cin >> Mobile;
-            cout << endl;
-            cout << " Enter Your Age:";
-            cin >> Age;
-            cout << endl;
-            cout << " Enter Your Gender:";
-            cin >> Gender;
-            cout << endl;
-            cout << " Enter Your Monthly Salary :";
-            cin >> MonthlySalary;
-            cout << endl;
-            cout << " ###################################################" << endl;
-            cout << " Thank Your for Registor! Your Account is open in AIS Bank  " << endl;
-            cout << " Your Account Number is " << acno << " and Initial Balance is : " << balance << " Rs. " << endl;
-
+        cout << "Enter Account Number: ";
+        cin >> acno;
+        cout << "Enter Name: ";
+        cin >> name;
+        cout << "Enter  Balance: ";
+        cin >> balance;
     }
     void ShowAccount()
     {
@@ -87,59 +82,38 @@ int Bank::Search(int a)
     return (0);
 }
 
-void showMenu()
-{
-    cout << "####################################################" << endl;
-    cout << "######         *Welcome to AIS ATM*           ######" << endl;
-    cout << "##  -------------------------------------------   ##" << endl;
-    cout << "##  1) Register a user / Account Open             ##" << endl;
-    cout << "##  2) Withdraw INR                               ##" << endl;
-    cout << "##  3) Withdraw International Currency            ##" << endl;
-    cout << "##  4) Check Balance                              ##" << endl;
-    cout << "####################################################" << endl;
-}
-
 // main code
 int main()
 {
     Bank C[10];
 
-    int found = 0, a, ch, i;
-    // for (i = 0; i <= 2; i++) {
-    //     C[i].OpenAccount();
-    // }
+    int uniq_id = 0, found = 0, a, ch, i;
 
     do
     {
         // display options
-        // cout << "\n\n1:Display All\n2:By Account No\n3:Deposit\n4:Withdraw\n5:Exit" << endl;
         showMenu();
 
         // user input
         cout << "Please input your choice: ";
         cin >> ch;
         system("cls");
-        
         switch (ch)
         {
-        case 1: // account open
-            // for (i = 0; i <= 2; i++)
-            // {
-            //     C[i].ShowAccount();
-            // }
-            C->OpenAccount();
+        case 0: // OPen Account
+            C[uniq_id].OpenAccount();
+            uniq_id++;
             break;
-        case 5: // account info
-            // for (i = 0; i <= 2; i++)
-            // {
-            //     C[i].ShowAccount();
-            // }
-            C->ShowAccount();
+        case 1: // displating account info
+            for (i = 0; i < uniq_id; i++)
+            {
+                C[i].ShowAccount();
+            }
             break;
         case 2: // searching the record
             cout << "Account Number? ";
             cin >> a;
-            for (i = 0; i <= 2; i++)
+            for (i = 0; i < uniq_id; i++)
             {
                 found = C[i].Search(a);
                 if (found)
@@ -151,7 +125,7 @@ int main()
         case 3: // deposit operation
             cout << "Account Number To Deposit Amount? ";
             cin >> a;
-            for (i = 0; i <= 2; i++)
+            for (i = 0; i < uniq_id; i++)
             {
                 found = C[i].Search(a);
                 if (found)
@@ -164,9 +138,9 @@ int main()
                 cout << "Record Not Found" << endl;
             break;
         case 4: // withdraw operation
-            cout << "Account Number To Withdraw Amount? ";
+            cout << "Enter Account Number To Withdraw Amount? ";
             cin >> a;
-            for (i = 0; i <= 2; i++)
+            for (i = 0; i < uniq_id; i++)
             {
                 found = C[i].Search(a);
                 if (found)
@@ -178,10 +152,12 @@ int main()
             if (!found)
                 cout << "Record Not Found" << endl;
             break;
-        case 6: // exit
+        case 5: // exit
             cout << "Have a nice day" << endl;
             break;
+        default:
+            cout << "Wrong Option" << endl;
         }
-    } while (ch != 10);
+    } while (ch != 5);
     return 0;
 }
